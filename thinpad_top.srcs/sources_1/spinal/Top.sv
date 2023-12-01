ee42a7bfd75e4998508af0f84fdb216dee4ec6d1
abmfy <wangbw21@mails.tsinghua.edu.cn>
2023-12-01 16:56:53 +0800

    feat: pre-commit hook to generate sv files

--- thinpad_top.srcs/sources_1/spinal/Top.sv
+++ thinpad_top.srcs/sources_1/spinal/Top.sv
@@ -1,6 +1,6 @@
 // Generator : SpinalHDL v1.9.4    git head : 270018552577f3bb8e5339ee2583c9c22d324215
 // Component : Top
-// Git hash  : 657828be6fff09218f7c8e7e05999dd7c047c990
+// Git hash  : 0bc6ad144b1c898387186f8e419e1d0cbb1399c0
 
 `timescale 1ns/1ps
 
