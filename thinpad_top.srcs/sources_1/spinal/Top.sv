2230dcb6864c35cca5c2f756abf71eed9b767989
abmfy <wangbw21@mails.tsinghua.edu.cn>
2023-12-09 19:48:27 +0800

    test: supervisor allowing longer timeout

--- thinpad_top.srcs/sources_1/spinal/Top.sv
+++ thinpad_top.srcs/sources_1/spinal/Top.sv
@@ -1,6 +1,6 @@
 // Generator : SpinalHDL v1.9.4    git head : 270018552577f3bb8e5339ee2583c9c22d324215
 // Component : Top
-// Git hash  : ff6cca3179b5f0d19314598efe932b42f1eb2459
+// Git hash  : 8f49bf00034f554e48c88e39fefe41ae602b4b18
 
 `timescale 1ns/1ps
 
