c82ee504222cec686bde0b346f95aa6bac7db136
abmfy <wangbw21@mails.tsinghua.edu.cn>
2023-12-14 04:51:33 +0800

    feat: dual sim tracer

--- spinal/tb/UCore.scala
+++ spinal/tb/UCore.scala
@@ -5,16 +5,19 @@ import cod.sim._
 import spinal.core._
 import spinal.core.sim._
 import spinal.lib.io._
+import spinal.lib.misc.test._
 
 import scala.util.Random
 import spinal.sim.SimThread
 
 object UCore extends App {
-    Config.sim.compile(InOutWrapper(new Top(
+    val compiled = Config.sim.compile(InOutWrapper(new Top(
         simulation = true,
         base_sram_init = Some("ucore/rbl.img"),
         ext_sram_init = Some("ucore/ucore_small.img"),
-    ))).doSim { dut =>
+    )))
+    
+    DualSimTracer(compiled, window = 1000000, seed = 42) { dut =>
         val period = (1 sec) / dut.simulation_freq
         val baud_period = (1 sec) / dut.uart.config.baud
 
@@ -28,8 +31,19 @@ object UCore extends App {
         dut.clockDomain.assertReset()
         sleep(baud_period * 10)
         dut.clockDomain.deassertReset()
+
+        var message = new StringBuilder
         
-        UartModel.decoder(dut.io.uart0.txd, baud_period)()
+        UartModel.decoder(dut.io.uart0.txd, baud_period) { char =>
+            message += char
+            if (message.compare("Load page fault") == 0) {
+                simFailure("Load page fault")
+            }
+            if (char == '\n') {
+                println(message)
+                message.clear()
+            }
+        }
 
         Tracer.init(dut.Wb, period)
 
